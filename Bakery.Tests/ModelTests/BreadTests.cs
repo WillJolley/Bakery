0f6797dfe0a62bc15b2a53726990380e561c0baa
WillJolley <yeswilljolley@gmail.com>
2023-09-09 18:54:36 -0700

    add bread constructor, first test passing

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -11,7 +11,7 @@ namespace Bakery.Tests
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
       Bread newBread = new Bread();
-      Assert.AreEqual(typeof(BreadTests), newBread.GetType());
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
   }
 }
