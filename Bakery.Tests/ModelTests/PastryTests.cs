7ca1d8a87879a801fc80bc42cf9a6c0e4365ab23
WillJolley <yeswilljolley@gmail.com>
2023-09-09 22:53:06 -0700

    add get pastries test, red phase

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -10,10 +10,17 @@ namespace Bakery.Tests
     [TestMethod]
     public void PastryConstructor_CreatesInstancesOfPastry_Pastry()
     {
-      {
-        Pastry newPastry = new Pastry(1);
-        Assert.AreEqual(typeof(Pastry), newPastry.GetType());
-      }
+      Pastry newPastry = new Pastry(1);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+
+    [TestMethod]
+    public void GetPastries_ReturnsPastries_Int()
+    {
+      int unit = 3;
+      Pastry newPastry = new Pastry(unit);
+      int result = newPastry.Unit;
+      Assert.AreEqual(unit, result);
     }
   }
 }
