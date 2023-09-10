859e5aab9ddce4c756d8393541e918971a1f8159
WillJolley <yeswilljolley@gmail.com>
2023-09-09 18:52:07 -0700

    constructor test red phase

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -1,11 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery.Models;
+using System.Collections.Generic;
 
 namespace Bakery.Tests
 {
   [TestClass]
   public class BreadTests
   {
-    
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(BreadTests), newBread.GetType());
+    }
   }
 }
