4e81ea16d2ecd23b5038fbff96b0396eaae0e313
WillJolley <yeswilljolley@gmail.com>
2023-09-14 21:49:56 -0700

    add pastry cost calculator method, test passing

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -5,10 +5,30 @@ namespace Bakery.Models
   public class Pastry
   {
     public int Unit { get; set; }
+    public int PastryCost { get; set; }
+    public List<int> pastryList = new List <int> {};
 
     public Pastry(int unit)
     {
       Unit = unit;
+      PastryCost = 0;
+    }
+
+    public int CalcCost()
+    {
+      while (Unit >= 0) 
+      {
+        pastryList.Add(+ Unit--);
+      }
+
+      pastryList.RemoveAll(element => element % 4 == 0);
+
+      foreach (int element in pastryList)
+      {
+        PastryCost += 2;
+      }
+
+      return PastryCost;
     }
   }
 }
